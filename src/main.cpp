878fa76110d69ea0a8e152e57dfea1397990afaf
human-gd <rezerve643@gmail.com>
2026-08-17 12:52:25 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
+using namespace geode::prelude;
+
+class $modify(BolonliPlayLayer, PlayLayer) {
+    void destroyPlayer(PlayerObject* player, GameObject* object) {
+        if (Mod::get()->getSettingValue<bool>("no-death")) {
+            // Встроенная функция GD для игнорирования урона.
+            this->toggleIgnoreDamage(true);
+            return;
+        }
+
+        PlayLayer::destroyPlayer(player, object);
+    }
+};
